dfdbf23cd245ba0b2d9e4f66d53769934c425e29
Joe Stanley <engineerjoe440@yahoo.com>
2023-12-24 11:08:31 -0800

    added soundux startup

--- launch_recording_session.py
+++ launch_recording_session.py
@@ -14,6 +14,14 @@ from configure_routes import PipeWireSession
 REAPER_TEMPLATE = "MultiChannelRecording.RPP"
 TEMPLATE_PATH = Path("~/.config/REAPER/ProjectTemplates/").expanduser()
 
+SOUNDUX_SCRIPT_PATH = (
+    "/home/joestan/stanleypadconfig/StreamDeck/launch_soundux.sh"
+)
+
+
+def launch_soundux():
+    """Launch the Soundux Application."""
+    subprocess.Popen([SOUNDUX_SCRIPT_PATH])
 
 def launch_reaper():
     """Launch the Reaper Application with the Appropriate Template."""
@@ -30,6 +38,7 @@ def launch_easy_effects():
 
 
 if __name__ == "__main__":
+    launch_soundux()
     launch_easy_effects()
     launch_reaper()
     time.sleep(5)
