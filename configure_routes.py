798d897770aced11963cf424a93aa237741fad06
Joe Stanley <engineerjoe440@yahoo.com>
2024-04-12 22:34:19 -0700

    improved to handle soundux connection

--- configure_routes.py
+++ configure_routes.py
@@ -12,6 +12,7 @@ ANALOG_INPUT_DEVICE_NAME = "alsa_input.usb-BurrBrown_from_Texas_Instruments_USB_
 ANALOG_OUTPUT_DEVICE_NAME = "alsa_output.usb-BurrBrown_from_Texas_Instruments_USB_AUDIO_CODEC-00.analog-stereo"
 EASY_EFFECTS_INPUT_DEVICE_NAME = "easyeffects_sink"
 EASY_EFFECTS_OUTPUT_DEVICE_NAME = "ee_soe_output_level"
+SOUNDUX_SINK_OUTPUT_DEVICE_NAME = "soundux_sink"
 ZOOM_DEVICE_NAME = "ZOOM VoiceEngine"
 
 
@@ -31,6 +32,7 @@ class PipeWireSession:
         self.connect_digital_input()
         self.connect_easy_effects_zoom()
         self.connect_easy_effects_output()
+        self.connect_soundux_input()
 
     def disconnect_default_connections(self):
         """Disconnect the Default Reaper and Zoom Connections."""
@@ -70,61 +72,101 @@ class PipeWireSession:
 
     def connect_analog_input(self):
         """Connect the Analog Input."""
-        analog_left = pw.Output(
+        reaper_port = self.reaper_devices[0] or pw.Input(
+            device=REAPER_DEVICE_NAME,
+            name="in1",
+            id=0,
+            port_type=pw.PortType.INPUT
+        )
+        pw.Output(
             device=ANALOG_INPUT_DEVICE_NAME,
             name="capture_FL",
             id=0,
             port_type=pw.PortType.OUTPUT
-        )
-        analog_right = pw.Output(
+        ).connect(reaper_port)
+        pw.Output(
             device=ANALOG_INPUT_DEVICE_NAME,
             name="capture_FR",
             id=0,
             port_type=pw.PortType.OUTPUT
-        )
-        analog_left.connect(self.reaper_devices[0])
-        analog_right.connect(self.reaper_devices[0])
+        ).connect(reaper_port)
 
     def connect_digital_input(self):
         """Connect the Digital Input."""
-        self.digital_left.connect(self.reaper_devices[1])
-        self.digital_right.connect(self.reaper_devices[1])
+        try:
+            reaper_port = self.reaper_devices[1] or pw.Input(
+                device=REAPER_DEVICE_NAME,
+                name="in2",
+                id=0,
+                port_type=pw.PortType.INPUT
+            )
+            self.digital_left.connect(reaper_port)
+            self.digital_right.connect(reaper_port)
+        except AttributeError:
+            print("WARNING: Failed to connect Digital Input.")
+
+    def connect_soundux_input(self):
+        """Connect the Soundux Interface."""
+        reaper_port = self.reaper_devices[2] or pw.Input(
+            device=REAPER_DEVICE_NAME,
+            name="in3",
+            id=0,
+            port_type=pw.PortType.INPUT
+        )
+        pw.Output(
+            device=SOUNDUX_SINK_OUTPUT_DEVICE_NAME,
+            name="monitor_FL",
+            id=0,
+            port_type=pw.PortType.OUTPUT
+        ).connect(reaper_port)
+        pw.Output(
+            device=SOUNDUX_SINK_OUTPUT_DEVICE_NAME,
+            name="monitor_FR",
+            id=0,
+            port_type=pw.PortType.OUTPUT
+        ).connect(reaper_port)
 
     def connect_easy_effects_zoom(self):
         """Connect the Zoom Output to Easy Effects if Zoom is Already Started."""
-        if self.zoom_output_left is None or self.zoom_output_right is None:
-            return
-        easy_effects_left = pw.Input(
-            device=EASY_EFFECTS_INPUT_DEVICE_NAME,
-            name="playback_FL",
-            id=0,
-            port_type=pw.PortType.INPUT
-        )
-        easy_effects_right = pw.Input(
-            device=EASY_EFFECTS_INPUT_DEVICE_NAME,
-            name="playback_FR",
-            id=0,
-            port_type=pw.PortType.INPUT
-        )
-        easy_effects_left.connect(self.zoom_output_left)
-        easy_effects_right.connect(self.zoom_output_right)
+        try:
+            if self.zoom_output_left is None or self.zoom_output_right is None:
+                return
+            easy_effects_left = pw.Input(
+                device=EASY_EFFECTS_INPUT_DEVICE_NAME,
+                name="playback_FL",
+                id=0,
+                port_type=pw.PortType.INPUT
+            )
+            easy_effects_right = pw.Input(
+                device=EASY_EFFECTS_INPUT_DEVICE_NAME,
+                name="playback_FR",
+                id=0,
+                port_type=pw.PortType.INPUT
+            )
+            easy_effects_left.connect(self.zoom_output_left)
+            easy_effects_right.connect(self.zoom_output_right)
+        except AttributeError:
+            print("WARNING: Failed to connect Easy Effects to Zoom.")
 
     def connect_easy_effects_output(self):
         """Connect the Easy Effects Output to the Appropriate DAC."""
-        analog_output_left = pw.Input(
-            device=ANALOG_OUTPUT_DEVICE_NAME,
-            name="playback_FL",
-            id=0,
-            port_type=pw.PortType.INPUT
-        )
-        analog_output_right = pw.Input(
-            device=ANALOG_OUTPUT_DEVICE_NAME,
-            name="playback_FR",
-            id=0,
-            port_type=pw.PortType.INPUT
-        )
-        analog_output_left.connect(self.digital_left)
-        analog_output_right.connect(self.digital_right)
+        try:
+            analog_output_left = pw.Input(
+                device=ANALOG_OUTPUT_DEVICE_NAME,
+                name="playback_FL",
+                id=0,
+                port_type=pw.PortType.INPUT
+            )
+            analog_output_right = pw.Input(
+                device=ANALOG_OUTPUT_DEVICE_NAME,
+                name="playback_FR",
+                id=0,
+                port_type=pw.PortType.INPUT
+            )
+            analog_output_left.connect(self.digital_left)
+            analog_output_right.connect(self.digital_right)
+        except AttributeError:
+            print("WARNING: Failed to connect Easy Effects to Zoom.")
 
 
 if __name__ == "__main__":
